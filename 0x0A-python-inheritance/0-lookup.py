1eff545196ffd4136ab27482502e4ea52c8e5caa
RIKO-99 <princericko@gmai.com>
2023-01-09 21:09:49 +0000

    lookup function

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+
 """
 Definition of a function lookup
+
 """
 
 def lookup(obj):
-    """Return a list of attributes and methods of an object
+    """
+    Return a list of attributes and methods of an object
     
     obj(any): objects whosse attributes and methods are to be returned
 
     """
-    print(dir(obj))
+    return dir(obj)
