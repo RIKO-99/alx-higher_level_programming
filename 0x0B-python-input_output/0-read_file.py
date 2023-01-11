6f5668c15f5a8a0bb9fe3fe6c1edf14bb04f48b3
RIKO-99 <princericko@gmai.com>
2023-01-11 21:39:08 +0000

    files with json

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 
-"""
-    contains function reads a text file
-
+"""Contains function reads a text file
 """
 
 def read_file(filename=""):
-    """
-    reads a text file
+    """Reads a text file
     """
     with open(filename, encoding="utf-8") as f:
         readfile = f.read()
