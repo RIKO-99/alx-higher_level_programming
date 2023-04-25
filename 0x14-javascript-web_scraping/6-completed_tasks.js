1c5f54ed9d6bc80accbb515f27052fda6c5dea75
RIKO-99 <rikohssn@gmail.com>
2023-04-25 18:54:30 +0000

    task completer by user

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+// a script to count items form an api call
+
+const request = require('request');
+const url = process.argv[2];
+request(url, (err, res, body) => {
+  if (err) console.error(err);
+  const tasks = JSON.parse(body);
+
+//initialize an empty object. 
+  const dict = {};
+
+//iterate over the array.
+  for (let i = 0; i < tasks.length; i++) {
+    if (!dict[tasks[i].userId]) {
+      dict[tasks[i].userId] = 0;
+    }
+    if (tasks[i].completed === true) {
+      dict[tasks[i].userId] += 1;
+    }
+  }
+  console.log(dict);
+});
